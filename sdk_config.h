5017095f19bfc72f84c760609c100fd27fa3b7db
Olivier Basille <obasille@hotmail.com>
2021-11-19 11:19:46 +0100

    Temporarily reduce the application data area to 4k due to the increase in size of the firmware

--- sdk_config.h
+++ sdk_config.h
@@ -1289,7 +1289,7 @@
 // <i> firmware upgrade. The size must be a multiple of the flash page size.
 
 #ifndef NRF_DFU_APP_DATA_AREA_SIZE
-#define NRF_DFU_APP_DATA_AREA_SIZE 8192
+#define NRF_DFU_APP_DATA_AREA_SIZE 4096
 #endif
 
 // <q> NRF_DFU_IN_APP  - Specifies that this code is in the app, not the bootloader, so some settings are off-limits.
